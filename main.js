0e8d51932322036a35d694043bc5ed682bdf23a6
Serafin <leaf466@gmail.com>
2016-11-15 16:57:49 -0800

    Adding class logic for cost

--- main.js
+++ main.js
@@ -4,19 +4,52 @@ let dom_errorMessage = document.getElementById("errorMessage")
 let dom_upgradeCost_1 = document.getElementById("upgradeCost_1")
 let dom_counter = document.getElementById("counter")
 
+// two point seven, year $20 president got elected (Andrew Jackson), 45 degree triangle, fibonaci, degrees in circle, to eat an airplane -> gets you 27 digits of euler's number
+const E = 2.71828182845904523536028747
+
+class Cost = {
+  constructor (cost) {
+    this.cost = cost
+    this.deltaCost = cost
+  }
+
+  increase() {
+    this.deltaCost += this.cost * 0.1
+    this.cost += this.deltaCost
+  }
+
+  get cost() {
+    return this.cost
+  }
+}
+
+let cycle = (prev, count) => {
+  if(count > 0) {
+    let newVal = COST_DELTA(prev)
+    cycle(newVal, --count )
+    console.log(newVal)
+  }
+}
+
 console.log( dom_counter )
 
 class Upgrade {
-  constructor(cost, func) {
-    this.cost = cost
+  constructor(cost, img, func) {
+    this.cost = new Cost(cost)
+    this.img = img
     this.me = func
+  }
+
+  handleCost() {
+    count -= this.cost
+    dom_counter.innerHTML = count
+    this.cost.increase()
   }
 }
 
 const increaseUpgrade = (upgrade) => {
   if(count >= upgrade.cost) {
-    count -= upgrade.cost
-    dom_counter.innerHTML = count
+    upgrade.handleCost()
     upgrade.me()
   }
   else {
@@ -24,16 +57,16 @@ const increaseUpgrade = (upgrade) => {
   }
 }
 
-const upgrade1 = new Upgrade( 5, () => {
-  upgrade1.cost += 5
-  dom_upgradeCost_1.innerHTML = upgrade1.cost
+const upgradeWire = new Upgrade( 5, './wire.jpg', () => {
+  upgradeWire.cost += 5
+  dom_upgradeCost_1.innerHTML = upgradeWire.cost
   countBy++
 })
 
 // Initialization
 if (typeof(Storage) !== "undefined") {
     dom_errorMessage.innerHTML = ""
-    dom_upgradeCost_1.innerHTML = upgrade1.cost
+    dom_upgradeCost_1.innerHTML = upgradeWire.cost
 
     $("#cookie").click( () => {
       count += countBy
@@ -41,7 +74,7 @@ if (typeof(Storage) !== "undefined") {
     })
 
     $("#upgrade_1").click( () => {
-      increaseUpgrade(upgrade1)
+      increaseUpgrade(upgradeWire)
     })
 
 } else {
