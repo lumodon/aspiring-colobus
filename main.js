a9b6310bf98a9bcfc04df6940d6894c7f17c16b2
Serafin <leaf466@gmail.com>
2016-11-17 14:38:22 -0800

    WIP: Implementing timer

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let count = 0
 let countBy = 1
 let dom_errorMessage = document.getElementById("errorMessage")
-let upgradeList = []
 let dom_counter = document.getElementById("counter")
+let dom_result = document.getElementById("result")
 
 // two point seven, year $20 president got elected (Andrew Jackson), 45 degree triangle, fibonaci, degrees in circle, to eat an airplane -> gets you 27 digits of euler's number
 
@@ -38,7 +38,6 @@ class Upgrade {
     this.$dom_img = $("<img src="+this.img+">")
     this.$dom_cost = $('<p id="upgrade'+this.upgName+'"></p>')
 
-    upgradeList.push(this)
     let $tempDiv = $("<div></div>").appendTo("#upgrades")
     this.$dom_img.click( () => {
       increaseUpgrade(this)
@@ -79,27 +78,55 @@ const upgradeWire = new Upgrade( 'Wire', './wire.jpg', 5,
   }
 )
 
-const upgradeNuclear = new Upgrade( 'Nuclear Plant', './nuclear.png', 500,
+const upgradeNuclear = new Upgrade( 'Nuclear Plant', './nuclear.png', 50,
   () => {
     timer.addPoint(upgradeNuclear.upgName)
   }
 )
+// This upgrade works on a timer. TODO: Organize to be self-implementing
+timer.upgradePoints.push(upgradeNuclear.upgName)
 
 let timer = {
-  upgradePoints: () => {
+  upgradePoints: [],
+  intervalTimer: undefined,
+  timerCount: 0,
+  // Public Methods
+  addPoint: (upgradeName) => {
+    timer.upgradePoints[upgradeName]++
+    return timer
+  },
+  enableTimer: () => {
+    timer.intervalTimer = window.setInterval(timer.tick, 1000)
+    return timer
+  },
+  disableTimer: () => {
+    if(timer.intervalTimer !== undefined) {
+      window.clearInterval( timer.intervalTimer )
+      return timer
+    }
+  },
+
+  // Private Methods
+  Initialization: () => {
     upgradeList.forEach( (index) => {
-      this[index] = 0
+      timer.upgradePoints[index.upgName] = 0
     })
   },
-  addPoint: (upgradeName) => {
-    upgradePoints[upgradeName]++
+  tick: () => {
+
+    dom_result.innerHTML = "Value: " + timer.timerCount++
   },
+
 }
+
+
 
 // Initialization
 if (typeof(Storage) !== "undefined") {
     dom_errorMessage.innerHTML = ""
     dom_counter.innerHTML = count
+
+    timer.Initialization()
 
     $("#cookie").click( () => {
       count += countBy
